e955b6c84d9575a4f8f259e651813d78d0951da2
aditya <adityapandeyadp@gmail.com>
2022-06-24 22:27:19 +0530

    main file is added

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
-sudo apt install git
+# sudo apt install git
 
-git clone https://github.com/Adityaadpandey/Linux-Package-Installer.git
-sudo apt install python3-pip
-python3 Linux-Package-Installer/run.py
+# git clone https://github.com/Adityaadpandey/Linux-Package-Installer.git
+
+# sudo apt install python3-pip
+
+sh -c "$(curl -fsSl https://raw.githubusercontent.com/Adityaadpandey/Linux-Package-Installer/master/main.py)"
+
+
+python3 Linux-Package-Installer/main.py
+
+rm -r Linux-Package-Installer
