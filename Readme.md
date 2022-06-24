426cb8cf0a5386619adde8af7ac406ece9f579cd
aditya <adityapandeyadp@gmail.com>
2022-06-24 22:30:30 +0530

    change in readme

--- Readme.md
+++ Readme.md
@@ -6,3 +6,7 @@ This is basically a python file to get things download and update any linux dist
 ## To Install this is using Curl
     
     sh -c "$(curl -fsSl https://raw.githubusercontent.com/Adityaadpandey/Linux-Package-Installer/master/install.sh)"
+
+## To Run this direclty from the terminal
+    
+    python3 -c "$(curl -fsSl https://raw.githubusercontent.com/Adityaadpandey/Linux-Package-Installer/master/main.py)"
--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 # sudo apt install git
 
-# git clone https://github.com/Adityaadpandey/Linux-Package-Installer.git
+git clone https://github.com/Adityaadpandey/Linux-Package-Installer.git
 
 # sudo apt install python3-pip
 
-sh -c "$(curl -fsSl https://raw.githubusercontent.com/Adityaadpandey/Linux-Package-Installer/master/main.py)"
+# sh -c "$(curl -fsSl https://raw.githubusercontent.com/Adityaadpandey/Linux-Package-Installer/master/main.py)"
 
 
 python3 Linux-Package-Installer/main.py
