7da206f6474498f77147187baa70504f6e3d2e55
aditya <adityapandeyadp@gmail.com>
2022-06-24 23:12:45 +0530

    sh file changed

--- install.sh
+++ install.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+filename=Linux-Package-Installer
+if test -f "$filename";
+then
+    python3 Linux-Package-Installer/main.py
+    sudo rm -r Linux-Package-Installer
 
-sudo apt install git
-
-git clone https://github.com/Adityaadpandey/Linux-Package-Installer.git
-
-# sudo apt install python3-pip
-
-# sh -c "$(curl -fsSl https://raw.githubusercontent.com/Adityaadpandey/Linux-Package-Installer/master/main.py)"
-
-
-python3 Linux-Package-Installer/main.py
+else
+    sudo apt install git
+    git clone https://github.com/Adityaadpandey/Linux-Package-Installer.git
+    sudo apt install python3-pip
+    python3 Linux-Package-Installer/main.py
+    sudo rm -r Linux-Package-Installer
+fi
