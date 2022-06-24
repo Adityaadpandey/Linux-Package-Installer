d8dd16c02ef9c103ab36954e9a8d976ca2c817a8
aditya <adityapandeyadp@gmail.com>
2022-06-24 23:15:10 +0530

    fine

--- install.sh
+++ install.sh
@@ -2,13 +2,17 @@
 filename=Linux-Package-Installer
 if test -f "$filename";
 then
-    python3 Linux-Package-Installer/main.py
+    cd  Linux-Package-Installer/
+    python3 main.py
+    cd ..
     sudo rm -r Linux-Package-Installer
 
 else
     sudo apt install git
     git clone https://github.com/Adityaadpandey/Linux-Package-Installer.git
     sudo apt install python3-pip
-    python3 Linux-Package-Installer/main.py
+    cd  Linux-Package-Installer/
+    python3 main.py
+    cd ..
     sudo rm -r Linux-Package-Installer
 fi
