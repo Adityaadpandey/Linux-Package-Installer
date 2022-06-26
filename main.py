09f7995c4199ffa2f7662825fefb693d875f2ae3
aditya <adityapandeyadp@gmail.com>
2022-06-27 02:34:29 +0530

    kotlin chaged

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import distro
 from LinuxMint.main import main as lm
+from Ubuntu.main import main as lm1
 
 
 name = distro.linux_distribution()
@@ -8,3 +9,6 @@ name = distro.linux_distribution()
 if name[0] == "Linux Mint":
     # print("Linux Mint")
     lm()
+if name[0] == "Ubuntu":
+    # print("Linux Mint")
+    lm1()
