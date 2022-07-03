a69fd8130e7eda16d75ac29788b19daf5674b7d6
aditya <adityapandeyadp@gmail.com>
2022-07-04 02:19:31 +0530

    small apt list made

--- main.py
+++ main.py
@@ -3,35 +3,10 @@ from Apt.run import main as apt_main
 
 name = distro.linux_distribution()
 
+apt = ["Linux Mint", "Ubuntu", "Debian","Lubuntu","Xubuntu","Kubuntu","peppermint","Knoppix","bodhi linux", "Deepin"]
 
-if name[0] == "Linux Mint":
-    print("Linux Mint")
+if name[0] in apt:
+    print(name[0])
     apt_main()
-if name[0] == "Ubuntu":
-    print("Ubuntu")
-    apt_main()
-
-if name[0] == "Debian":
-    print("Debian")
-    apt_main()
-if name[0] == "Lubuntu":
-    print("Lubuntu")
-    apt_main()
-if name[0] == "Xubuntu":
-    print("Xubuntu")
-    apt_main()
-if name[0] == "Kubuntu":
-    print("Kubuntu")
-    apt_main()
-if name[0] == "peppermint":
-    print("peppermint")
-    apt_main()
-if name[0] == "Knoppix":
-    print("Knoppix")
-    apt_main()
-if name[0] == "bodhi linux":
-    print("bodhi linux")
-    apt_main()
-if name[0] == "Deepin":
-    print("Deepin")
-    apt_main()
+else:
+    print("Not Available for your OS")
